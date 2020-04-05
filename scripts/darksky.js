2fc4348ec8f6a1f27563cf7ac85ebace49049cb6
Jamie Hamilton <jamiehamilton81@gmail.com>
2020-04-05 16:59:28 +0100

    More recommendations added and pictures for all

--- scripts/darksky.js
+++ scripts/darksky.js
@@ -22,6 +22,7 @@ var thingstododata = [
     goodforpoorweather: true,
     lat: 52.5724835,
     long: -0.2413988,
+    image: "cathedral.jpg",
     description:
       "With one of the most dramatic West Fronts in the country, an extraordinary creation of medieval architecture, it would be easy for the interior to be an anticlimax, but it is not. The dramatic Romanesque interior is little altered since its completion 800 years ago and the whole building has recently undergone cleaning and restoration following the dramatic fire of November 2001. The Cathedral is a great place to visit for all ages. You can download an interactive trail with augmented reality animations for mobile phones and tablets (search for Gamar Ltd in your app store, then Peterborough Cathedral Trail), explorer backpacks designed for smaller children and an activity book for older children."
   },
@@ -33,6 +34,7 @@ var thingstododata = [
     goodforpoorweather: true,
     lat: 52.5760152,
     long: -0.240409,
+    image: "laserforce.jpg",
     description:
       "Live action laser game. Two floor space station maze. Aliens, lasers and interactive targets. Lots of fun and excitement."
   },
@@ -44,24 +46,26 @@ var thingstododata = [
     goodforpoorweather: true,
     lat: 52.6047047,
     long: -0.2780162,
+    image: "bounce.jpg",
     description:
       "Bounce is the 1st and original Indoor Trampoline Park in the UK! Launched on 31st May 2014, Bounce parks offer over 100 interconnected state of the art trampolines in over 20,000 sqft of entertainment space! Our Bounce parks consist of differing courts including our Main Arena, Dodgeball Court, Kids Court, Slam Dunk Area, Foam Pit, Airbag Jump, Touch Walls, Gladiator Pits and Tumble Tracks, as well as an arcade and party rooms!"
   },
 
   {
     name: "Queensgate Shopping Centre",
-    address: "Queensgate Centre, PETERBOROUGH, Cambridgeshire, PE1 1NT",
+    address: "Queensgate Centre, Peterborough, Cambridgeshire, PE1 1NT",
     type: "Shopping Centre",
     goodforpoorweather: true,
     lat: 52.5738054,
     long: -0.2449751,
+    image: "queensgate.jpg",
     description:
       "Queensgate is Peterborough city centres main shopping centre, home to over 90 stores including John Lewis, M&S, Bhs, Next, Boots, H&M, Primark, Superdry, River Island, Monsoon, Paperchase and many more. If you're looking for somewhere to stop for a bite to eat or a coffee you'll find lots of choice in the centre, eateries include Handmade Burger Co and Carluccio's, Costa Coffee, Muffin Break, The John Lewis Place To Eat, Cafe Revive at M&S, McDonalds, Pret and Greggs."
   },
 
   {
     name: "Peterborough Lido Outdoor Swimming Pool",
-    address: "Bishops Road, PETERBOROUGH, Cambridgeshire, PE1 1YY",
+    address: "Bishops Road, Peterborough, Cambridgeshire, PE1 1YY",
     type: "Swimming Outdoor",
     goodforpoorweather: false,
     lat: 52.5701659,
@@ -74,7 +78,7 @@ var thingstododata = [
   {
     name: "Sacrewell",
     address:
-      "Great North Road, Thornhaugh, PETERBOROUGH, Cambridgeshire, PE8 6HJ",
+      "Great North Road, Thornhaugh, Peterborough, Cambridgeshire, PE8 6HJ",
     type: "Farm",
     goodforpoorweather: false,
     lat: 52.5892333,
@@ -86,7 +90,7 @@ var thingstododata = [
 
   {
     name: "Railworld Wildlife Haven",
-    address: "Oundle Road, PETERBOROUGH, Cambridgeshire, PE2 9NR",
+    address: "Oundle Road, Peterborough, Cambridgeshire, PE2 9NR",
     type: "Miniature Railway",
     goodforpoorweather: false,
     lat: 52.5680372,
@@ -94,6 +98,77 @@ var thingstododata = [
     image: "railworld.jpg",
     description:
       "The Railworld Wildlife Haven is testament to what can be achieved when volunteers, companies, groups and individuals work together for a common goal – Encouraging Environmental Awareness. Rev Richard Paten our founder chair and life long volunteer – brought the land over 30 years ago and we have raised funds to create what we have today.... We have never had any core funding, but we have won six major UK Environmental Awards and one Worldwide Award from Caterpillar."
+  },
+
+  {
+    name: "Flag Fen Archaeological Park",
+    address:
+      "Flag Fen Archaeological Park, The Droveway, Northey Road, Peterborough, Cambridgeshire, PE6 7QJ",
+    type: "Historical Site",
+    goodforpoorweather: false,
+    lat: 52.574683,
+    long: -0.1918993,
+    image: "flagfen.jpg",
+    description:
+      "Visit Flag Fen Archaeology Park to explore how the prehistoric people of the fen lived over 3000 years ago. Wander through a Bronze Age village, sit within the reconstructed roundhouses, and stand where our ancestors once stood by the ritual causeway. Experience life in our prehistoric past and visit the only place in the UK where original Bronze Age remains can be seen in situ, the incredibly preserved timbers of a monumental engineering achievement. Excavations on the site revealed details of a wooden platform and post alignment that stretch for nearly a kilometre across the fen. These were built up between 1350 and 950BC and are of great national and international significance. Due to the waterlogged nature of the fens, this unique monument has been remarkably preserved."
+  },
+
+  {
+    name: "Central Park",
+    address: "Park Crescent, Peterborough, Cambridgeshire, PE1 4DZ",
+    type: "Country Park",
+    goodforpoorweather: false,
+    lat: 52.5842088,
+    long: -0.2375554,
+    image: "centralpark.jpg",
+    description:
+      "Central Park, situated on Park Crescent, is the council's most prestigious park in the city. Several areas of the park have been refurbished over the past few years, including the sunken garden, the sensory garden and two play areas. There is also a new sand-pit and soft surfacing around the paddling pool. Other facilities at the park include an aviary, bowling greens, hard and grass tennis courts, a putting green, a croquet lawn and, last but not least, the popular Buttercross Tea-rooms! Many events are held in the park throughout the year."
+  },
+
+  {
+    name: "Elton Hall",
+    address: "Elton, Peterborough, Cambridgeshire, PE8 6SH",
+    type: "Historic House",
+    goodforpoorweather: false,
+    lat: 52.5238629,
+    long: -0.3986596,
+    image: "eltonhall.jpg",
+    description:
+      "The Hall has been home to the Proby family since 1660.  It is an enchanting house which has evolved throughout the centuries.  Every room contains magnificent treasures, from late 15th century Old Masters, to Reynolds, Constable and remarkable Victorian painters such as Millais and Alma Tadema.  Each generation has collected books and there are three libraries containing over 10,000 books. One of the most remarkable is Henry VIII’s prayer book with inscriptions by him and his three children. The garden has been lovingly restored over the last 35 years with mature topiary, a Gothic Orangery and billowing flower borders set between immaculately cut hedges."
+  },
+
+  {
+    name: "Peterborough Museum and Art Gallery",
+    address: "Priestgate, Peterborough, Cambridgeshire, PE1 1LF",
+    type: "Museum",
+    goodforpoorweather: true,
+    lat: 52.5717777,
+    long: -0.2458579,
+    image: "museum.jpg",
+    description:
+      "Located in one of the city's most historic buildings, Peterborough Museum has a wealth of stories to fascinate and enthral all the family. There are some amazing objects and interactive displays for all ages. The collections comprise over 200,000 items of great national and international importance."
+  },
+
+  {
+    name: "Longthorpe Tower",
+    address: "Thorpe Road, Longthorpe, Peterborough, Cambridgeshire, PE3 6LU",
+    type: "Historic Site",
+    goodforpoorweather: true,
+    lat: 52.5708228,
+    long: -0.2890745,
+    image: "longthorpe.jpg",
+    description: "Situated in a quiet suburb of Peterborough is this delightful medieval tower, part of a manor house that was built around 1300. Inside you will discover a hidden gem, because the main chamber is covered with an incredible set of wall paintings from the 14th century. Leading experts have listed Longthorpe Tower as one of the 100 most important historic sites in the UK."
+  },
+
+  {
+    name: "The Key Theatre",
+    address: "Embankment Rd, Peterborough, PE1 1EF",
+    type: "Theatre",
+    goodforpoorweather: true,
+    lat: 52.5685745,
+    long: -0.2410498,
+    image: "keytheatre.jpg",
+    description: "Situated in Peterborough's city centre the Key Theatre brings great entertainment to the area with the theatre programme made up of 'home-grown' productions, national touring shows, local community productions and a full programme of one-off concerts. Also part of the Key Theatre is ‘Riva’ the fabulous restaurant offering excellent food, plus views of the Nene embankment, making this a stunning setting for city centre dining. The Key’s pantomimes are renowned throughout the region for their quality and traditional sense of good, clean, family fun. And with over 30 years of experience it’s no wonder that thousands of children have grown up enjoying panto at the Key and many now bring their own families too!"
   }
 ];
 
@@ -225,6 +300,12 @@ request.onload = function() {
         titlecontainer.appendChild(title);
         cardcontainer.appendChild(titlecontainer);
 
+        const typecontainer = document.createElement("h4");
+        const type = document.createElement("i");
+        type.textContent = item.type;
+        typecontainer.appendChild(type);
+        cardcontainer.appendChild(typecontainer);
+
         const description = document.createElement("p");
         item.description = item.description.substring(0, 300);
         description.textContent = `${item.description}...`;
@@ -262,7 +343,7 @@ request.onload = function() {
         const img = document.createElement("img");
         img.src = "images/thingstodo/" + item.image;
         img.setAttribute("style", "width:100%;");
-        
+
         card.appendChild(img);
 
         const cardcontainer = document.createElement("div");
@@ -275,6 +356,12 @@ request.onload = function() {
         title.textContent = item.name;
         titlecontainer.appendChild(title);
         cardcontainer.appendChild(titlecontainer);
+
+        const typecontainer = document.createElement("h4");
+        const type = document.createElement("i");
+        type.textContent = item.type;
+        typecontainer.appendChild(type);
+        cardcontainer.appendChild(typecontainer);
 
         const description = document.createElement("p");
         item.description = item.description.substring(0, 300);
