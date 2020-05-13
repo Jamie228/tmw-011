35aca6c7992443650e9a4d1d587149717c9e05c3
Jamie Hamilton <jamiehamilton81@gmail.com>
2020-05-13 11:42:39 +0100

    STarted air quality info

--- scripts/pboinfo.js
+++ scripts/pboinfo.js
@@ -15,24 +15,45 @@ $(document).ready(function () {
         if (darkskyrequest.status >= 200 && darkskyrequest.status < 400) {
             const weathercard = document.getElementById("weatherinfo");
             const temp = document.createElement("h3");
-            temp.textContent = "Current Temperature: " + darkskydata.currently.temperature + "°C";
+            temp.textContent =
+                "Current Temperature: " + darkskydata.currently.temperature + "°C";
             weathercard.appendChild(temp);
             const feelslike = document.createElement("h3");
-            feelslike.textContent = "Feels Like: " + darkskydata.currently.apparentTemperature + "°C";
+            feelslike.textContent =
+                "Feels Like: " + darkskydata.currently.apparentTemperature + "°C";
             weathercard.appendChild(feelslike);
             const hourlySum = document.createElement("h3");
             hourlySum.textContent = "Hourly Summary: " + darkskydata.hourly.summary;
             weathercard.appendChild(hourlySum);
             const precip = document.createElement("h3");
-            precip.textContent = "Precipitation Probability: " + (darkskydata.currently.precipProbability * 100) + "%";
+            precip.textContent =
+                "Precipitation Probability: " +
+                darkskydata.currently.precipProbability * 100 +
+                "%";
             weathercard.appendChild(precip);
             const uv = document.createElement("h3");
             uv.textContent = "UV Index: " + darkskydata.currently.uvIndex;
             weathercard.appendChild(uv);
-        } else {
-
-        }
-    };
-
+        } else {}
+    }
     darkskyrequest.send();
+
+    var airqualityrequest = new XMLHttpRequest();
+    airqualityrequest.open(
+        "GET",
+        "https://api.breezometer.com/air-quality/v2/current-conditions?lat=52.5695&lon=-0.2405&key=5b5963bd0ebf4a25905e20be69ca3f83&features=breezometer_aqi,local_aqi,sources_and_effects,health_recommendations,pollutants_concentrations",
+        true
+    );
+
+    airqualityrequest.onload = function () {
+        var airqualdata = JSON.parse(this.response);
+        if (airqualityrequest.status >= 200 && airqualityrequest.status < 400) {
+            const airqualcard = document.getElementById("airquality");
+            const aqistatus = document.createElement("h3");
+            aqistatus.textContent = "Daily Air Quality Index (DAQI): " + airqualdata.data.indexes.gbr_defra.aqi + " - " + airqualdata.data.indexes.gbr_defra.category;
+            airqualcard.style = "border-color: " + airqualdata.data.indexes.baqi.color;
+            airqualcard.appendChild(aqistatus);
+        }
+    }
+    airqualityrequest.send();
 });
